aae6c99e3f0aa51f8184b61c413ad372a0735d81
ngsneto@gmail.com <silvinha1234567>
2019-09-09 09:05:54 -0300

    Alguns gráficos

---
+++ barra.py
@@ -0,0 +1,19 @@
+
+
+import statistics
+
+import matplotlib.pyplot as plt
+
+# Definindo variáveis
+x = ["doce"]
+y = ["nenhum","doce"]
+
+x2 = ["salgado"]
+y2 = ["salgado"]
+
+# Criando um gráfico
+plt.bar(x, y, label='doce', color='r')
+plt.bar(x2, y2, label='salgado', color='y')
+plt.legend(loc=9)
+
+plt.show()
