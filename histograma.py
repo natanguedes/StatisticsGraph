cb09ca80a567e64b3d6bb32b281a300ca6498d51
ngsneto@gmail.com <silvinha1234567>
2019-09-10 10:28:24 -0300

    Alguns gráficos

---
+++ histograma.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('C:/Users/Natanael/PycharmProjects/untitled3/questionario.csv')
+
+"""
+Criando um diagrama de barras para os totais de cada idade
+"""
+idade = df['Idade']
+idade_sum = idade.value_counts()
+idade_sum = idade_sum.sort_index()
+
+x = idade_sum.index
+y = idade_sum
+
+plt.figure(figsize=(8, 6))
+
+"""
+sobre a função bar(*args, **kwargs)
+Parameters:
+
+x:  As coordenadas x das barras
+y:  A altura (s) das barras
+
+Mais em:
+https://matplotlib.org/api/_as_gen/matplotlib.pyplot.bar.html
+"""
+plt.bar(x, y)
+
+plt.title('Distribuição de Idades')
+plt.xlabel('Idade')
+plt.ylabel('Alunos')
+plt.xticks(x) # obriga a mostrar todos os números no eixo x
+plt.show()
+plt.savefig('C:/Users/Natanael/PycharmProjects/untitled3/diagrama-barras.png')
+plt.close()
