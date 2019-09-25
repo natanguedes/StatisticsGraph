9b0515ff4c22588999bfbd8be73bac915d06134b
ngsneto@gmail.com <silvinha1234567>
2019-09-25 11:34:53 -0300

    Alguns gráficos

---
+++ csvread.py
@@ -0,0 +1,9 @@
+import csv
+import matplotlib.pyplot as plt
+# arquivo = open('Ano-2012.csv',encoding="utf8")
+#
+# linha = csv.reader(arquivo)
+
+
+df = csv.reader('Ano-2012.csv')
+
--- pie.py
+++ pie.py
@@ -1,8 +1,18 @@
 
 import matplotlib.pyplot as plt
-fatias = [7, 2, 2, 13]
-atividades = ['dormir','comer','trabalhar','passear']
-colunas = ['c','m','r','k']
+# fatias = [90, 5]
+# atividades = ['F','M']
+# colunas = ['c','k']
+#
+# plt.pie(fatias, labels = atividades, colors = colunas, startangle = 90, shadow = True, explode = (0,0.1))
+# plt.show()
+# # F,F,M,M,F,M,F,F,F,F,F,F,F,M,F,F,F,M,F,F,F,F,F,F,F
 
-plt.pie(fatias, labels = atividades, colors = colunas, startangle = 90, shadow = True, explode = (0,0.1,0,0))
+values = [90, 10]
+colors = ['b', 'g']
+labels = ['Feminino', 'Masculino']
+xplode = (0.2, 0)
+plt.pie(values, colors=colors, labels=values, explode=xplode, counterclock=False, shadow=True)
+plt.title('Relacao entre feminino e masculino')
+plt.legend(labels, loc=3)
 plt.show()
