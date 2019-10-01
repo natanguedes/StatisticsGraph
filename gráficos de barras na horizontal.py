d9c0a3c38b0825a6d4b10f2760a1ed83e3e4b9a5
ngsneto@gmail.com <silvinha1234567>
2019-10-01 10:23:20 -0300

    Alguns gráficos

---
+++ gráficos de barras na horizontal.py
@@ -0,0 +1,12 @@
+
+import matplotlib.pyplot as plt
+
+Produtos = ['trigo','farinha','arroz','macarrao','mandioca']
+qtdade = [40,22,33,40,70]
+
+plt.barh(Produtos,qtdade, color='purple')
+plt.ylabel("Produtos agriculas")
+plt.xlabel("Produtos em %")
+plt.title("Produto com maior participação na industria agricola")
+plt.show()
+
