7b6af36ef589fb769cfcc063af40f8ecff2b7bb9
ngsneto@gmail.com <silvinha1234567>
2019-10-02 09:53:42 -0300

    Alguns gráficos

--- Pareto.py
+++ Pareto.py
@@ -2,8 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import PercentFormatter
 
-df = pd.DataFrame({'country': [177.0, 7.0, 4.0, 2.0, 2.0, 1.0, 1.0, 1.0]})
-df.index = ['USA', 'Canada', 'Russia', 'UK', 'Belgium', 'Mexico', 'Germany', 'Denmark']
+df = pd.DataFrame({'country': [177.0, 7.0, 4.0, 2.0, 2.0, 1.0]})
+df.index = ['Quarto', 'Acessório', 'Limpeza', 'Conforto', 'Qualidade da alimentação', 'Outro']
 df = df.sort_values(by='country',ascending=False)
 df["cumpercentage"] = df["country"].cumsum()/df["country"].sum()*100
 
@@ -13,7 +13,11 @@ ax.bar(df.index, df["country"], color="C0")
 ax2 = ax.twinx()
 ax2.plot(df.index, df["cumpercentage"], color="C1", marker="D", ms=7)
 ax2.yaxis.set_major_formatter(PercentFormatter())
+plt.xlabel('t (s)')
 
+plt.ylabel('Reclamações')
+
+plt.title("Gráfico de reclamações")
 ax.tick_params(axis="y", colors="C0")
 ax2.tick_params(axis="y", colors="C1")
 plt.show()
