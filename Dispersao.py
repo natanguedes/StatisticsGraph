69f88ef3ba216eecdab3c768486283d594aa3e82
ngsneto@gmail.com <silvinha1234567>
2019-10-02 18:06:27 -0300

    Alguns gráficos

--- Dispersao.py
+++ Dispersao.py
@@ -5,9 +5,9 @@ from bokeh.sampledata.iris import flowers
 colormap = {'setosa': 'red', 'versicolor': 'green', 'virginica': 'blue'}
 colors = [colormap[x] for x in flowers['species']]
 
-p = figure(title = "Iris Morphology")
-p.xaxis.axis_label = 'Petal Length'
-p.yaxis.axis_label = 'Petal Width'
+p = figure(title = "Número de manutenções por dia")
+p.xaxis.axis_label = 'Dias da semana'
+p.yaxis.axis_label = 'Número de manutenções'
 p.circle(flowers["petal_length"], flowers["petal_width"], color=colors, fill_alpha=0.2, size=10)
 
 show(p)
