f719a6ea8210ff1e28e2472f802e53cedbfb5a43
ngsneto@gmail.com <silvinha1234567>
2019-10-03 17:36:36 -0300

    Alguns gráficos

--- Graficodepontos.py
+++ Graficodepontos.py
@@ -7,8 +7,8 @@ data1 = [10,5,2,4,6,8]
 data2 = [ 1,2,4,8,7,4]
 x = 10*np.array(range(len(data1)))
 
-plt.plot( x, data1, 'go') # green bolinha
-plt.plot( x, data1, 'k:', color='orange') # linha pontilha orange
+# plt.plot( x, data1, 'go') # green bolinha
+# plt.plot( x, data1, 'k:', color='orange') # linha pontilha orange
 
 plt.plot( x, data2, 'r^') # red triangulo
 plt.plot( x, data2, 'k--', color='blue')  # linha tracejada azul
