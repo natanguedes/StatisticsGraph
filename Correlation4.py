9046560f79100406412717cb9923b7ec9e2ef3a6
ngsneto@gmail.com <silvinha1234567>
2019-10-03 09:22:15 -0300

    Alguns gráficos

--- Correlation4.py
+++ Correlation4.py
@@ -3,7 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
-plt.plot([10,20,30,40], [15, 40, 75, 90], linestyle='--', color='r', marker='s',
-         linewidth=3.0)
+plt.plot([10,20,30,40], [15, 40, 75, 90],   linestyle='--', color='r', marker='s',
+         linewidth=3.0, )
 plt.axis([0,50,0,100])
+plt.xlabel("teste")
+plt.ylabel("teste2")
+plt.title("exemplo de correlação")
 plt.show()
