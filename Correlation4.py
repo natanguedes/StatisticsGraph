1734e16f0aa5d63c36eb138dfb40ff3dafa67649
ngsneto@gmail.com <silvinha1234567>
2019-10-02 22:47:45 -0300

    Alguns gráficos

---
+++ Correlation4.py
@@ -0,0 +1,9 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+plt.plot([10,20,30,40], [15, 40, 75, 90], linestyle='--', color='r', marker='s',
+         linewidth=3.0)
+plt.axis([0,50,0,100])
+plt.show()
