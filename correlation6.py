fb66e32736c4391fb643d2116c4b666067e41e1d
ngsneto@gmail.com <silvinha1234567>
2019-10-09 18:16:52 -0300

    Alguns gráficos

--- correlation6.py
+++ correlation6.py
@@ -11,11 +11,12 @@ df['b'] = 100 - df['a'] + np.random.normal(0, 50, 1000) # negatively correlated 
 df['c'] = 100 - df['a'] + np.random.normal(0, 50, 1000) # negatively correlated with 'a'
 df['d'] = 100 - df['a'] + np.random.normal(0, 50, 1000) # negatively correlated with 'a'
 plt.matshow(df.corr())
+plt.title("exemplo de correlação\n\n")
+
 plt.xticks(range(len(df.columns)), df.columns)
 plt.yticks(range(len(df.columns)), df.columns)
 plt.colorbar()
+
 plt.show()
 s= df.corr()
 print(s)
-
-plt.title("exemplo de correlação")
